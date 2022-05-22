a244393dec461046fa4208304b971fc8d90260f1
ziminliu <liuziminliuzimin@gmail.com>
2022-05-22 18:47:29 +0800

    feat:19.删除链表的倒数第-n-个结点

---
+++ 19.删除链表的倒数第-n-个结点.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    // 哨兵节点
+    const dummyNode = new ListNode(null, head);
+    let left = dummyNode;
+    let right = dummyNode;
+    for (let i = 0; i < n; i++) {
+        right = right.next;
+    }
+    while (right.next) {
+        left = left.next;
+        right = right.next;
+    }
+    left.next = left.next.next;
+    return dummyNode.next;
+};
+// @lc code=end
+
