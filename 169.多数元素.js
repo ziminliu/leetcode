b7218e323488e69997c79eb79ada0d3644d7f792
ziminliu <liuziminliuzimin@gmail.com>
2022-05-22 18:45:15 +0800

    feat: 169.返回数组中的众数

---
+++ 169.多数元素.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    return  nums.sort()[Math.floor(nums.length/2)]
+};
+// @lc code=end
+
---
+++ jianzhi/39.数组中出现次数超过一半的数字.js
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn
+ * 剑指 Offer 39. 数组中出现次数超过一半的数字 
+ * url https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement = function(nums) {
+
+};
