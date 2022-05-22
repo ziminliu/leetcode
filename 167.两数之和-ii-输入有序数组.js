fb1da3ce1153ee955ebb7e6c45e1ebbd9df206c9
ziminliu <liuziminliuzimin@gmail.com>
2022-05-22 18:46:55 +0800

    feat: 167.两数之和-ii-输入有序数组

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let start = 0;
+  let end = numbers.length - 1;
+  while (start < end && numbers[start] + numbers[end] !== target) {
+    if (numbers[start] + numbers[end] > target) {
+      end--;
+    } else {
+      start++;
+    }
+  }
+  return [start + 1, end + 1];
+};
+// @lc code=end
