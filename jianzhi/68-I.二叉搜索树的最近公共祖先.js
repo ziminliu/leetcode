3e1d80a12c5846182ec5a020100274098bebe271
ziminliu <liuziminliuzimin@gmail.com>
2022-05-22 18:46:26 +0800

    feat: 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

---
+++ jianzhi/68-I.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function (root, p, q) {
+    if (root) {
+        // 在左边
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q)
+        } else if (p.val > root.val && q.val > root.val) {
+            // 在右边
+            return lowestCommonAncestor(root.right, p, q)
+        } else {
+            // 分叉
+            return root;
+        }
+    }
+    return null;
+};
