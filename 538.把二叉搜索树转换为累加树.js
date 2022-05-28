cdde4246b978fde59a8de6f445cad37a853c5b8d
ziminliu <liuziminliuzimin@gmail.com>
2022-05-28 19:47:38 +0800

    feat: 538.把二叉搜索树转换为累加树 && 1038.从二叉搜索树到更大和树

---
+++ 538.把二叉搜索树转换为累加树.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=javascript
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function (root) {
+  // 逆中序遍历
+  let sum = 0;
+  function dfs(root) {
+    if (root) {
+      dfs(root.right);
+      root.val += sum;
+      sum = root.val;
+      dfs(root.left);
+    }
+  }
+  dfs(root);
+  return root;
+};
+// @lc code=end
